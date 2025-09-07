d712f05ab58f2787184ad631debb3268c53e2734
Paulo Victor Pinheiro <pauovictorpinheiro998663264@gmail.com>
2025-09-07 01:55:20 -0300

    implement sqlite

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from doctest import debug
 from flask import Flask, jsonify
+from repository.database import db
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECREAT_KEY'] = 'SECREAT_KEY_WEBSOCKET'
+with app.app_context():
+    db.init_app(app)
+    db.create_all()
+    db.session.commit()
 @app.route('/payments/pix', methods=["POST"])
 def create_paymetn_pix():
     return jsonify({"message": "The payment has been created"})
