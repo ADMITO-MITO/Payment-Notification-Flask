462d4caf838f0815d69722e7bf3ee56990c77cbd
Paulo Victor Pinheiro <pauovictorpinheiro998663264@gmail.com>
2025-09-08 00:18:54 -0300

    Create qrcode on static

--- models/payments/pix.py
+++ models/payments/pix.py
@@ -1,27 +1,25 @@
 import uuid
 import qrcode
 import os
+
 class Pix:
     def __init__(self) -> None:
         pass
 
     def create_payment(self, base_path=''):
-        # Create payment from financial institution
         bank_payment_id = str(uuid.uuid4())
 
-        # QR code
         hash_payment = f'hash_payment_{bank_payment_id}'
         img = qrcode.make(hash_payment)
 
-        # Caminho para salvar a imagem
         filename = f"qr_code_payment_{bank_payment_id}.png"
         filepath = os.path.join(base_path, 'static', 'image', filename)
 
-        # Criar diretório se não existir
         os.makedirs(os.path.dirname(filepath), exist_ok=True)
 
-        # Salvar imagem
-        img.save(filepath)#type:ignore
+
+        img.save(filepath) #type:ignore
+        print(f"QR code salvo em: {filepath}")  #debug
 
         return {
             "bank_payment_id": bank_payment_id,
